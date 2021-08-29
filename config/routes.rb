32aa8059a5a7cb52c4ee09688b8a335ef2e03040
Sajan <sajanbasnet75@gmail.com>
2021-08-29 19:35:30 +0545

    edited routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,7 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      get 'users/index'
-      get 'users/show'
-      get 'users/create'
-      get 'users/update'
-      get 'users/delete'
+      resources :users
     end
   end
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
