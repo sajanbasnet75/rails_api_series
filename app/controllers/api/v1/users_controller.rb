2632d7bab9ffd090d9147c8c920b62ad6e7e153e
Sajan <sajanbasnet75@gmail.com>
2021-09-11 11:25:51 +0545

    fixup! fixup! add json api serializer

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -2,7 +2,7 @@
 
 module Api
   module V1
-    class UsersController < ApplicationController
+    class UsersController < BaseController
       def index
         render jsonapi: User.all
       end
